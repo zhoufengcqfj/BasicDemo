b35ff0951300c50e8a2782c2a8c7fc05b99ca025
zhoufeng <17671461759@163.com>
2020-01-16 11:47:51 +0800

    多渠道打包

--- app/src/main/java/com/zf/myapplication/MainActivity.java
+++ app/src/main/java/com/zf/myapplication/MainActivity.java
@@ -2,6 +2,12 @@ package com.zf.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.meituan.android.walle.WalleChannelReader;
+import com.zf.utils.ChannelUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("afddadf", ChannelUtil.getChannel(this,"123"));
+    }
+
+    public void test1(View view) {
+        String channel = WalleChannelReader.getChannel(this.getApplicationContext());
+        Toast.makeText(this, ChannelUtil.getChannel(this,"123--"+channel),Toast.LENGTH_SHORT).show();
     }
 }
---
+++ readme.md
@@ -0,0 +1,2 @@
+生成渠道包 ./gradlew clean assembleReleaseChannels
+支持 productFlavors ./gradlew clean assembleMeituanReleaseChannels
